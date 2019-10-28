a2f3f2f664739ff2186dbfd1b456b456ceaafabf
Anthony <anthony.fernandes@slaom.com>
2019-10-27 20:23:07 -0700

    Sample code for Dot Net React Js

---
+++ dotnet-react/ReactJs/ReactJs/wwwroot/js/react-hello-world.js
@@ -0,0 +1,14 @@
+﻿var e = React.createElement;
+
+class HelloWorld extends React.Component {
+    render() {
+        return e(
+            "div",
+            null,
+            "Hello World from internal React Js"
+        );
+    }
+}
+
+const containerElement = document.getElementById('content');
+ReactDOM.render(e(HelloWorld), containerElement);
